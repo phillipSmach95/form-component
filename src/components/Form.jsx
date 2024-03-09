434a326953a2756cba8f34d4b2c5ff1bb6420551
phillipSmach95 <smach.phillip@gmail.com>
2024-03-09 16:51:28 +0100

    - form action is missing - style not done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,57 +2,157 @@ import { useState } from "react";
 import "./Form.css";
 
 function Form() {
-    const [course, setCourse]=useState("java");
-    const [courseDate, setCourseDate]=useState(new Date());
-    const [beginner, setBeginner]=useState("");
-    const [advaneced, setAdvanced]=useState("");
-    const [pro, setPro]=useState("");
-    const [level, setLevel]=useState("beginner");
-    const [name, setName]=useState('');
-    const [lastname, setLastname]=useState("");
-    const [email, setEmail]=useState('');
-    const [street, setStreet]=useState("");
-    const [plz,setPlz]=useState(0);
-    const [city, setCity]=useState("");
+  const today = new Date();
+  const [course, setCourse] = useState("java");
+  const [courseDate, setCourseDate] = useState(today);
+  const [beginner, setBeginner] = useState(false);
+  const [advaneced, setAdvanced] = useState(false);
+  const [pro, setPro] = useState(false);
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [street, setStreet] = useState("");
+  const [plz, setPlz] = useState(0);
+  const [city, setCity] = useState("");
+  const onOptionSelect = (course) => {
+    setCourse(course);
+  };
+  const onBeginnerToggle = () => {
+    setBeginner(!beginner);
+    setAdvanced(false);
+    setPro(false);
+  };
+  const onAdvancedToggle = () => {
+    setAdvanced(!advaneced);
+    setBeginner(false);
+    setPro(false);
+  };
+  const onProToggle = () => {
+    setPro(!pro);
+    setBeginner(false);
+    setAdvanced(false);
+  };
+
+  function sendForm(formData) {
+    const data = formData.get("name");
+    console.log(data);
+  }
 
   return (
-    <div>
-      <form action="" method="post">
+      <form action={sendForm}>
         <label htmlFor="course">Kurs</label>
-        <select name="course" id="course">
-          <option value={()=>setCourse('java')}>Java</option>
-          <option value={()=>setCourse('Python')}>Python</option>
-          <option value={()=>setCourse('JavaScript')}>JavaScript</option>
+        <select
+          name="course"
+          id="course"
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        >
+          <option value="Java" onSelect={(e) => onOptionSelect(e.target.value)}>
+            Java
+          </option>
+          <option
+            value="Python"
+            onSelect={(e) => onOptionSelect(e.target.value)}
+          >
+            Python
+          </option>
+          <option
+            value="JavaScript"
+            onSelect={(e) => onOptionSelect(e.target.value)}
+          >
+            JavaScript
+          </option>
         </select>
+        <p>{course}</p>
         <label htmlFor="date">Datum</label>
-        <input type="date" name="date" id="date" />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          value={courseDate}
+          onChange={(e) => setCourseDate(e.target.value)}
+        />
         <div className="radio-form-style">
           <label htmlFor="beginner">Anfänger</label>
-          <input type="radio" name="beginner" id="beginner" />
+          <input
+            type="radio"
+            name="beginner"
+            id="beginner"
+            checked={beginner}
+            onChange={() => onBeginnerToggle()}
+          />
           <label htmlFor="advanced">Fortgeschritten</label>
-          <input type="radio" name="advanced" id="advanced" />
+          <input
+            type="radio"
+            name="advanced"
+            id="advanced"
+            checked={advaneced}
+            onChange={() => onAdvancedToggle()}
+          />
           <label htmlFor="pro">Professionell</label>
-          <input type="radio" name="pro" id="pro" />
+          <input
+            type="radio"
+            name="pro"
+            id="pro"
+            checked={pro}
+            onChange={() => onProToggle()}
+          />
         </div>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" onChange={(e)=>setName(e.target.value)}/>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <p>{name}</p>
           <label htmlFor="lastname">Nachname:</label>
-          <input type="text" name="lastname" id="lastname" onChange={(e)=> setLastname(e.target.value)} />
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
           <label htmlFor="email">Email:</label>
-          <input type="text" name="email" id="email" onChange={(e)=> setEmail(e.target.value)}/>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="street">Strasse und Nummer:</label>
-          <input type="text" name="street" id="street" onChange={(e)=> setStreet(e.target.value)}/>
+          <input
+            type="text"
+            name="street"
+            id="street"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+          />
           <label htmlFor="zipcode">PLZ:</label>
-          <input type="text" name="zipcode" id="zipcode" onChange={(e)=> setPlz(e.target.value)}/>
+          <input
+            type="text"
+            name="zipcode"
+            id="zipcode"
+            value={plz}
+            onChange={(e) => setPlz(e.target.value)}
+          />
           <label htmlFor="city">Stadt:</label>
-          <input type="text" name="city" id="city" onChange={(e)=> setCity(e.target.value)} />
+          <input
+            type="text"
+            name="city"
+            id="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
         </div>
+        <button type="submit">Absenden</button>
       </form>
-    </div>
   );
 }
 export default Form;
